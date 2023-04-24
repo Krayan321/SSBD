befe6ab768cfd629e67b21a64cd2c88243db28b9
236641 Rubacha Jan <236641@edu.p.lodz.pl>
2023-04-24 21:26:02 +0200

    SSBD202301-57 database structures are being generated

--- init_db_perms.sql
+++ init_db_perms.sql
@@ -1,8 +1,114 @@
-create user ssbd01admin password 'admin';
 create user ssbd01glassfish password 'glassfishpassword';
 create user ssbd01mok password 'mokpassword';
 create user ssbd01moa password 'moapassword';
 
-create database ssbd01;
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
 
-grant all on schema public to ssbd01admin;
+
+--
+-- Name: TABLE access_level; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT,INSERT,UPDATE ON TABLE public.access_level TO ssbd01mok;
+GRANT SELECT ON TABLE public.access_level TO ssbd01moa;
+
+
+--
+-- Name: TABLE account; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.account TO ssbd01mok;
+GRANT SELECT ON TABLE public.account TO ssbd01moa;
+
+
+--
+-- Name: TABLE admin_data; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT,INSERT,UPDATE ON TABLE public.admin_data TO ssbd01mok;
+GRANT SELECT ON TABLE public.admin_data TO ssbd01moa;
+
+
+--
+-- Name: TABLE category; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT ON TABLE public.category TO ssbd01mok;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.category TO ssbd01moa;
+
+
+--
+-- Name: TABLE chemist_data; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT,INSERT,UPDATE ON TABLE public.chemist_data TO ssbd01mok;
+GRANT SELECT ON TABLE public.chemist_data TO ssbd01moa;
+
+
+--
+-- Name: TABLE glassfish_auth_view; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT ON TABLE public.glassfish_auth_view TO ssbd01glassfish;
+
+
+--
+-- Name: TABLE medication; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT ON TABLE public.medication TO ssbd01mok;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.medication TO ssbd01moa;
+
+
+--
+-- Name: TABLE order_medication; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT ON TABLE public.order_medication TO ssbd01mok;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.order_medication TO ssbd01moa;
+
+
+--
+-- Name: TABLE patient_data; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.patient_data TO ssbd01mok;
+GRANT SELECT ON TABLE public.patient_data TO ssbd01moa;
+
+
+--
+-- Name: TABLE patient_order; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT ON TABLE public.patient_order TO ssbd01mok;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.patient_order TO ssbd01moa;
+
+
+--
+-- Name: TABLE prescription; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT ON TABLE public.prescription TO ssbd01mok;
+GRANT SELECT,INSERT,DELETE ON TABLE public.prescription TO ssbd01moa;
+
+
+--
+-- Name: TABLE shipment; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT ON TABLE public.shipment TO ssbd01mok;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.shipment TO ssbd01moa;
+
+
+--
+-- Name: TABLE shipment_medication; Type: ACL; Schema: public; Owner: ssbd01admin
+--
+
+GRANT SELECT ON TABLE public.shipment_medication TO ssbd01mok;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.shipment_medication TO ssbd01moa;
+
+
+--
+-- PostgreSQL database dump complete
+--
