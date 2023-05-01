09f3c9758931b4a1793afe213bba40506964af4d
236641 Rubacha Jan <236641@edu.p.lodz.pl>
2023-05-01 22:19:56 +0200

    SSBD202301-63 merge fixes returns

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd01/security/JwtUtils.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd01/security/JwtUtils.java
@@ -3,13 +3,20 @@ package pl.lodz.p.it.ssbd2023.ssbd01.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import jakarta.inject.Inject;
 import jakarta.security.enterprise.identitystore.CredentialValidationResult;
 import java.time.ZonedDateTime;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Set;
+
+import lombok.extern.java.Log;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.crypto.SecretKey;
+
+@Log
 public class JwtUtils {
 
     @Inject
@@ -25,17 +32,26 @@ public class JwtUtils {
         String principal = result.getCallerPrincipal().getName();
         Set<String> authorities = result.getCallerGroups();
 
+        System.out.println(SECRET);
+        log.warning(SECRET);
+
         return Jwts.builder()
                 .setSubject(principal)
                 .claim("roles", String.join(",", authorities))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
                 .setExpiration(new Date(ZonedDateTime.now().plusMinutes(TIMEOUT).toInstant().toEpochMilli()))
+                .signWith(getSecretKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
+    public SecretKey getSecretKey(){
+        byte[] encodeKey = Base64.getDecoder().decode(SECRET);
+        return Keys.hmacShaKeyFor(encodeKey);
+    }
+
     public Claims load(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET)
+        return Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
