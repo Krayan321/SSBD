710856dfc682ece928873442bbbfbf8405973f5d
236641 Rubacha Jan <236641@edu.p.lodz.pl>
2023-05-02 00:28:17 +0200

    SSBD202301-63 added filter for spa applications

---
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd01/config/SpaFilter.java
@@ -0,0 +1,37 @@
+package pl.lodz.p.it.ssbd2023.ssbd01.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.java.Log;
+
+import java.io.IOException;
+
+@Log
+public class SpaFilter implements Filter {
+    private FilterConfig config;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        this.config = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String uri = request.getRequestURI();
+
+        if (uri.matches("^/[a-zA-Z0-9\\-_]+$")) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
+            dispatcher.forward(servletRequest, servletResponse);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        config = null;
+        Filter.super.destroy();
+    }
+}
