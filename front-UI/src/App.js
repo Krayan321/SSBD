0ac106fddd3922336c614c0c2ffde8b8804ffe61
Krayan321 <krayan321@gmail.com>
2023-05-05 00:28:21 +0200

    SSBD202301-89 - added sign up form and basic http methods

--- front-UI/src/App.js
+++ front-UI/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
+import './App.css';
 import './App.css';
 import Login from './pages/Login';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Navbar from './components/Navbar'
+import SingUp from './pages/SignUp';
+import AllAccounts from './pages/AllAccounts';
+import SingleAccount from './pages/SingleAccount';
+import AccountInfo from './pages/AccountInfo';
+import EditSingleAccount from './pages/EditSingleAccount';
 
 
 function App() {
@@ -11,8 +16,13 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SingUp />} />
+        <Route path="/accounts" element={<AllAccounts />} />
+        <Route path="/accounts:id" element={<SingleAccount />} />
+        <Route path="/accounts/self" element={<AccountInfo />} />
+        <Route path="/accounts/edit/:id" element={<EditSingleAccount />} />
       </Routes>
     </Router>
   );
--- front-UI/src/pages/SignUp.js
+++ front-UI/src/pages/SignUp.js
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid, Paper, TextField, Button , Box} from '@mui/material';
 
 function SignUp() {
+    const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
+    const headerStyle = { margin: 0 }
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [login, setLogin] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [pesel, setPesel] = useState("");
+    const [nip, setNip] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+      };
+
     return (
-        <div>
-            
-        </div>
-    );
+        <Grid>
+            <Paper elevation={20} sx={paperStyle}>
+                <h2 style={headerStyle}>Sign Up</h2>
+                <Box sx={{display: "flex", flexDirection: "column", width: "60%" }} onSubmit={handleSubmit}>
+                    <TextField sx={{ width: 300}}
+                        fullWidth 
+                        label='Name' 
+                        placeholder="Enter your name"
+                        onChange={(event) => setName(event.target.value)}
+                        />
+                    <TextField sx={{ width: 300}}
+                        fullWidth 
+                        label='LastName' 
+                        placeholder="Enter your last name" 
+                        onChange={(event) => setLastName(event.target.value)}
+                        />
+                    <TextField sx={{ width: 300}}
+                        fullWidth 
+                        label='Login' 
+                        placeholder="Enter your login" 
+                        onChange={(event) => setLogin(event.target.value)}
+                        />
+                    <TextField sx={{ width: 300}}
+                        fullWidth 
+                        label='Email' 
+                        placeholder="Enter your email" 
+                        onChange={(event) => setEmail(event.target.value)}
+                        />
+                    <TextField sx={{ width: 300}}
+                        fullWidth 
+                        label='Password' 
+                        placeholder="Enter your password"
+                        onChange={(event) => setPassword(event.target.value)}
+                        />
+                    <TextField sx={{ width: 300}}
+                        fullWidth 
+                        label='Phone Number' 
+                        placeholder="Enter your phone number" 
+                        onChange={(event) => setPhoneNumber(event.target.value)}
+                        />
+                    <TextField sx={{ width: 300}}
+                        fullWidth 
+                        label='Pesel' 
+                        placeholder="Enter your pesel number"
+                        onChange={(event) => setPesel(event.target.value)}
+                        />
+                    <TextField sx={{ width: 300}}
+                        fullWidth 
+                        label='NIP' 
+                        placeholder="Enter your NIP number"
+                        onChange={(event) => setNip(event.target.value)}
+                        />
+                    <Button type='submit' variant='contained' color='primary'>Sign up</Button>
+                </Box>
+            </Paper>
+        </Grid>
+    )
 }
 
 export default SignUp;
