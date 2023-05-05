38dad0924b453536698f129dba1c271532d3395b
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-05-05 18:09:09 +0200

    SSBD202301-85 add work phone number to AdminData

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -123,4 +123,4 @@ insert into account (active, confirmed, created_by, creation_date, email, langua
 
 insert into access_level (account_id, active, created_by, creation_date, modification_date, modified_by, version, access_level_role) values (1, true, null, now(), null, null, 0, 'ADMIN');
 
-insert into admin_data (id) values (1);
+insert into admin_data (id, work_phone_number) values (1, "123456789");
