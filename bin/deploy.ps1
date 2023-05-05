6ed866053ce5fc0a20949cff191e7ea84abee0fa
236641 <236641@edu.p.lodz.pl>
2023-05-05 18:51:29 +0200

    SSBD202301-90 no hardcoded war file name

--- bin/deploy.ps1
+++ bin/deploy.ps1
@@ -5,8 +5,9 @@ Set-Location "$PROJECT_ROOT"
 Invoke-Expression -Command ".\bin\build_front.ps1"
 Invoke-Expression -Command ".\bin\build_backend.ps1"
 
-$WAR = Get-ChildItem -Path "$PROJECT_ROOT\target" *.war
+$WAR = Get-ChildItem -Path "$PROJECT_ROOT\target" *.war | Select-Object -First 1
 
 #asadmin $PAYARA_OPTIONS undeploy "ssbd01-0.0.1"
 #asadmin --host 10.31.201.3 --port 4848 --user admin --passwordfile "$PROJECT_ROOT\config\payara-password" deploy --name "ssbd01" "$PROJECT_ROOT\target\ssbd01-0.0.1.war"
-asadmin --host 10.31.201.3 --port 4848 --user admin --passwordfile "$PROJECT_ROOT\config\payara-password" redeploy --name "ssbd01" "$PROJECT_ROOT\target\ssbd01-0.0.1.war"
+
+asadmin --host 10.31.201.3 --port 4848 --user admin --passwordfile "$PROJECT_ROOT\config\payara-password" redeploy --name "ssbd01" "$PROJECT_ROOT\target\$WAR"
