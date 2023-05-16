b3cb26d11008e2795a345f17c54c1d9487f23eaf
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-05-16 20:47:06 +0200

    SSBD202301-115 add confirmed and active chemist account to init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -120,10 +120,13 @@ GRANT SELECT, UPDATE ON TABLE public.account_id_seq TO ssbd01mok;
 GRANT SELECT, UPDATE ON TABLE public.access_level_id_seq TO ssbd01mok;
 
 insert into account (active, confirmed, created_by, creation_date, email, language, last_negative_login, last_positive_login, logical_address, login,incorrect_login_attempts, modification_date, modified_by, password, version) values (true, true, null, now(), 'admin@o2.pl', 'en', null, null, null, 'admin123', 0, null, null,  'b03ddf3ca2e714a6548e7495e2a03f5e824eaac9837cd7f159c67b90fb4b7342', 0);
+insert into account (active, confirmed, created_by, creation_date, email, language, last_negative_login, last_positive_login, logical_address, login,incorrect_login_attempts, modification_date, modified_by, password, version) values (true, true, null, now(), 'chemist@o2.pl', 'en', null, null, null, 'chemist123', 0, null, null,  '52d7a0431ddd469b9e0929a09ef67fefe99e3511893edb0c2fa0b09892df1e52', 0);
 
 insert into access_level (account_id, active, created_by, creation_date, modification_date, modified_by, version, access_level_role) values (1, true, null, now(), null, null, 0, 'ADMIN');
+insert into access_level (account_id, active, created_by, creation_date, modification_date, modified_by, version, access_level_role) values (2, true, null, now(), null, null, 0, 'CHEMIST');
 
 insert into admin_data (id, work_phone_number) values (1, '123456789');
+insert into chemist_data (id, license_number) values (2, '456456');
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.token TO ssbd01mok;
 
