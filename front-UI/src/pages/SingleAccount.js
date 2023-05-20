b6277d4f83e28e4be7edd7941dfed059b46fdcdb
Jakub Glegola <krayan321@gmail.com>
2023-05-20 14:20:47 +0200

    SSBD202301-139 - setAccount not working properly

--- front-UI/src/pages/SingleAccount.js
+++ front-UI/src/pages/SingleAccount.js
@@ -7,50 +7,49 @@ import { Box, Typography } from "@mui/material";
 
 function SingleAccount() {
   const { id } = useParams();
-  const [account, setAccount] = useState([]);
+  const [account, setAccount] = useState("");
   const findAccount = useCallback(async () => {
   let response = await getAccountDetails(id);
-  console.log("response")
-  console.log(response);
-
   if (response.status === 200) {
-      setAccount(response.data);
-      console.log("response.data")
       console.log(response.data);
+      const aaa = JSON.stringify(response.data);
+      setAccount(aaa);
     } else {
       console.log('Nie udało się pobrać konta');
     }
+
+  console.log(account)
   }, []);
 
   useEffect(() => {
     findAccount();
   }, [findAccount]);
 
-  return (
-    <Box>
-      <Typography variant="h4" component="h4" gutterBottom>
-        Account details
-      </Typography>
-      <Typography variant="h6" component="h6" gutterBottom>
-        Actice: {account.active}
-      </Typography>
-      <Typography variant="h6" component="h6" gutterBottom>
-        Confirmed: {account.confirmed}
-      </Typography>
-      <Typography variant="h6" component="h6" gutterBottom>
-        Email: {account.email}
-      </Typography>
-      <Typography variant="h6" component="h6" gutterBottom>
-        Login: {account.login}
-      </Typography>
-      <Typography variant="h6" component="h6" gutterBottom>
-        Access level: {account}
-      </Typography>
+  // return (
+  //   <Box>
+  //     <Typography variant="h4" component="h4" gutterBottom>
+  //       Account details
+  //     </Typography>
+  //     <Typography variant="h6" component="h6" gutterBottom>
+  //       Actice: {account.active}
+  //     </Typography>
+  //     <Typography variant="h6" component="h6" gutterBottom>
+  //       Confirmed: {account.confirmed}
+  //     </Typography>
+  //     <Typography variant="h6" component="h6" gutterBottom>
+  //       Email: {account.email}
+  //     </Typography>
+  //     <Typography variant="h6" component="h6" gutterBottom>
+  //       Login: {account.login}
+  //     </Typography>
+  //     <Typography variant="h6" component="h6" gutterBottom>
+  //       Access level: {account}
+  //     </Typography>
 
       
-    </Box>
+  //   </Box>
 
-  )
+  // )
 }
 
 export default SingleAccount;
