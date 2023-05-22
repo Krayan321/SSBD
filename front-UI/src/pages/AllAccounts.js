a22091b05dd28bbd6301d17a1e22aea564fb575d
Jakub Glegola <krayan321@gmail.com>
2023-05-23 00:33:04 +0200

    SSBD202301-154 added tostify, working as intended

--- front-UI/src/pages/AllAccounts.js
+++ front-UI/src/pages/AllAccounts.js
@@ -18,17 +18,24 @@ import {
   DialogTitle,
   DialogActions,
   DialogContent,
+  DialogContentText,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import LockIcon from "@mui/icons-material/Lock";
 import LockOpenIcon from "@mui/icons-material/LockOpen";
+import { useTranslation } from "react-i18next";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function AllAccounts() {
   const [accounts, setAccounts] = useState([]);
   const navigate = useNavigate();
 
-  const [open, setOpen] = useState(false);
-  const [selectedId, setSelectedId] = useState(null);
+  const [dialogStates, setDialogStates] = useState({});
+
+  // const [open, setOpen] = useState(false);
+  // const [selectedId, setSelectedId] = useState(null);
+
+  const { t } = useTranslation();
 
   const findAccounts = useCallback(async () => {
     let response = await getAccounts();
@@ -37,7 +44,7 @@ export default function AllAccounts() {
       setAccounts(response.data);
       console.log(response.data);
     } else {
-      console.log("Nie udało się pobrać kont");
+      return <div>Nothing to show</div>;
     }
   }, []);
 
@@ -47,40 +54,79 @@ export default function AllAccounts() {
 
   const handleAccountDetails = async (accountId) => {
     const id = accountId;
-    const accountToUpdate = [...accounts];
-    const indexOfAccountToEdit = accountToUpdate.findIndex(
-      (account) => account.id === accountId
-    );
-    if (indexOfAccountToEdit !== -1) {
-      setAccounts(accountToUpdate);
-    }
     navigate(`/accounts/${id}/details`);
   };
 
-  const handleAccountBlock = async (active) => {
-    if (selectedId) {
+  const handleAccountBlock = async (active, accountId) => {
+    if (accountId) {
       const updatedAccount = accounts.map((account) =>
-        account.id === selectedId ? { ...account, active: !active } : account
+        account.id === accountId ? { ...account, active: !active } : account
       );
       setAccounts(updatedAccount);
       if (active) {
-        await blockAccount(selectedId);
-      }
-      if (!active) {
-        await unblockAccount(selectedId);
+        await blockAccount(accountId)
+          .then(() => {
+            toast.success(t("account_blocked"), {
+              position: "top-center",
+            });
+          })
+          .catch((error) => {
+            if (error.response.status === 401) {
+              toast.error(t("account_blocked_error"), {
+                position: "top-center",
+              });
+            } else if (error.response.status === 404) {
+              toast.error(t("account_blocked_error"), {
+                position: "top-center",
+              });
+            } else if (error.response.status === 500) {
+              toast.error(t("account_blocked_error"), {
+                position: "top-center",
+              });
+            }
+          });
+      } else {
+        await unblockAccount(accountId)
+          .then(() => {
+            toast.success(t("account_unblocked"), {
+              position: "top-center",
+            });
+          })
+          .catch((error) => {
+            if (error.response.status === 401) {
+              toast.error(t("account_unblocked_error"), {
+                position: "top-center",
+              });
+            } else if (error.response.status === 403) {
+              toast.error(t("account_unblocked_error"), {
+                position: "top-center",
+              });
+            } else if (error.response.status === 500) {
+              toast.error(t("account_unblocked_error"), {
+                position: "top-center",
+              });
+            }
+          });
       }
     }
-    setOpen(false);
+    setDialogStates((prevState) => ({
+      ...prevState,
+      [accountId]: false,
+    }));
   };
 
-  const handleCancel = () => {
-    setOpen(false);
+  const handleCancel = (accountId) => {
+    setDialogStates((prevState) => ({
+      ...prevState,
+      [accountId]: false,
+    }));
   };
 
-  const handleClick = (id) => {
-    setSelectedId(id);
-    console.log(id);
-    setOpen(true);
+  const handleClick = (accountId) => {
+    setDialogStates((prevState) => ({
+      ...prevState,
+      [accountId]: true,
+    }));
   };
 
   return (
@@ -111,17 +157,57 @@ export default function AllAccounts() {
                 <Button onClick={() => handleClick(row.id)}>
                   {row.active ? <LockOpenIcon /> : <LockIcon />}
                 </Button>
-                <Dialog open={open} onClose={handleCancel}>
-                  <DialogTitle>Are you sure?</DialogTitle>
-                  <DialogContent>
-                    Are you sure you want to change the state?
-                  </DialogContent>
-                  <DialogActions>
-                    <Button onClick={handleCancel}>Cancel</Button>
-                    <Button onClick={() => handleAccountBlock(row.active)}>
-                      Confirm
-                    </Button>
-                  </DialogActions>
+                <Dialog
+                  open={dialogStates[row.id] || false}
+                  onClose={() => handleCancel(row.id)}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                >
+                  {row.active ? (
+                    <div>
+                      <DialogTitle id="alert-dialog-title">
+                        {t("block_account?")}
+                      </DialogTitle>
+                      <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                          {t("block_account_description")}
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions>
+                        <Button onClick={() => handleCancel(row.id)}>
+                          Close
+                        </Button>
+                        <Button
+                          onClick={() => handleAccountBlock(row.active, row.id)}
+                          autoFocus
+                        >
+                          Block
+                        </Button>
+                      </DialogActions>
+                    </div>
+                  ) : (
+                    <div>
+                      <DialogTitle id="alert-dialog-title">
+                        {t("unblock_account?")}
+                      </DialogTitle>
+                      <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                          {t("unblock_account_description")}
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions>
+                        <Button onClick={() => handleCancel(row.id)}>
+                          Close
+                        </Button>
+                        <Button
+                          onClick={() => handleAccountBlock(row.active, row.id)}
+                          autoFocus
+                        >
+                          Unblock
+                        </Button>
+                      </DialogActions>
+                    </div>
+                  )}
                 </Dialog>
               </TableCell>
               <TableCell align="right">
@@ -133,6 +219,7 @@ export default function AllAccounts() {
           ))}
         </TableBody>
       </Table>
+      <ToastContainer />
     </TableContainer>
   );
 }
