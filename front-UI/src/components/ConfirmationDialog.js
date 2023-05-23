e8a165d325e00a36a23593a63264993f35cbf9aa
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-05-23 14:56:56 +0200

    SSBD202301-138 add generic confirmation dialog

---
+++ front-UI/src/components/ConfirmationDialog.js
@@ -0,0 +1,18 @@
+import {Dialog, DialogActions, DialogContent, DialogTitle} from "@mui/material";
+import Button from '@mui/material/Button';
+
+
+const ConfirmationDialog = ({open, title, actions, onClose}) => {
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogActions>
+                {actions.map((action) => (
+                    <Button color={action.color} onClick={action.handler}>{action.label}</Button>
+                ))}
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default ConfirmationDialog;
