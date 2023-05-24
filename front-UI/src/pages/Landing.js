8f24d5ea1b2bd9ea8db2d7c6f158354678926f7e
229979 <229979@edu.p.lodz.pl>
2023-05-24 11:41:11 +0200

    SSBD202301-157, przekierowanie z landing, formularz do refactoru

--- front-UI/src/pages/Landing.js
+++ front-UI/src/pages/Landing.js
@@ -1,20 +1,28 @@
 import React from "react";
 import axios from "axios";
-import {useTranslation} from "react-i18next";
-import {Paper} from "@mui/material";
-import {useSelector} from "react-redux";
+import { useTranslation } from "react-i18next";
+import { Paper, Button } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Landing() {
-
     const user = useSelector((state) => state.user);
-    const {t} = useTranslation();
-    const paperStyle = {padding: '30px 20px', margin: "auto", width: 400}
+    const { t } = useTranslation();
+    const paperStyle = { padding: '30px 20px', margin: "auto", width: 400 };
+    const navigate = useNavigate();
+
+    const handleCreateAccount = () => {
+        navigate("/create-account");
+    };
 
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignContent: 'center', marginTop: '3rem'}}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', marginTop: '3rem' }}>
             <Paper elevation={20} style={paperStyle}>
-                <h2 style={{fontFamily: 'Lato'}}>
+                <h2 style={{ fontFamily: 'Lato' }}>
                     {t("welcome_back")} {user.sub} </h2>
+                <Button variant="contained" onClick={handleCreateAccount}>
+                    {t("create_account")}
+                </Button>
             </Paper>
         </div>
     );
