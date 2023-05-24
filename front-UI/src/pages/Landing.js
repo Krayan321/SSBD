044465401933ece8aff4dd775048b8634ad1e652
229979 <229979@edu.p.lodz.pl>
2023-05-24 23:47:10 +0200

    SSBD202301-169, Tylko Admin widzi utwórz konto.

--- front-UI/src/pages/Landing.js
+++ front-UI/src/pages/Landing.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 import { Paper, Button } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+
 
 function Landing() {
     const user = useSelector((state) => state.user);
     const { t } = useTranslation();
     const navigate = useNavigate();
+    const isAdmin = user.roles.includes("ADMIN");
+
+
 
     const handleCreateAccount = () => {
         navigate("/create-account");
@@ -17,11 +21,12 @@ function Landing() {
     return (
         <div className="wrapper">
             <Paper elevation={20} className="paper">
-                <h2>
-                    {t("welcome_back")} {user.sub} </h2>
-                <Button variant="contained" onClick={handleCreateAccount}>
-                    {t("create_account")}
-                </Button>
+                <h2>{t("welcome_back")} {user.sub} </h2>
+                {isAdmin  && (
+                    <Button variant="contained" onClick={handleCreateAccount}>
+                        {t("create_account")}
+                    </Button>)}
+
             </Paper>
         </div>
     );
