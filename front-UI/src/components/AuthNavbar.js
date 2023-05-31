0dbe19d7f2161629053ed29a4a9f212398c972e8
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-05-31 15:24:33 +0200

    SSBD202301-193 restructure auth layout

--- front-UI/src/components/AuthNavbar.js
+++ front-UI/src/components/AuthNavbar.js
@@ -5,7 +5,6 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
 import {useNavigate} from "react-router-dom";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
@@ -13,61 +12,20 @@ import i18n from "i18next";
 import LanguageIcon from "@mui/icons-material/Language";
 import {
     AccountCircle,
-    LoginTwoTone,
-    LogoDevOutlined,
-    Logout,
 } from "@mui/icons-material";
 import {useTranslation} from "react-i18next";
 import {useDispatch, useSelector} from "react-redux";
-import {changeLevel, logout} from "../redux/UserSlice";
-import {blue, pink, red, purple} from "@mui/material/colors";
-import ConfirmationDialog from "./ConfirmationDialog";
-import {useEffect, useState} from "react";
+import {changeLevel} from "../redux/UserSlice";
+import {useState} from "react";
 import {Pathnames} from "../router/Pathnames";
 import {ROLES} from "../constants/Constants";
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import {useLocation} from "react-router-dom";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
 import {languages} from "../constants/Constants";
 import {changeLanguage} from "../api/mok/accountApi";
+import LoopIcon from '@mui/icons-material/Loop';
 
-
-const guestTheme = createTheme({
-    palette: {
-        primary: blue,
-    },
-    typography: {
-        fontFamily: ["Lato", "sans-serif"].join(","),
-    },
-});
-
-const adminTheme = createTheme({
-    palette: {
-        primary: red,
-    },
-    typography: {
-        fontFamily: ["Lato", "sans-serif"].join(","),
-    },
-});
-
-const chemistTheme = createTheme({
-    palette: {
-        primary: purple,
-    },
-    typography: {
-        fontFamily: ["Lato", "sans-serif"].join(","),
-    },
-});
-
-const patientTheme = createTheme({
-    palette: {
-        primary: pink,
-    },
-    typography: {
-        fontFamily: ["Lato", "sans-serif"].join(","),
-    },
-});
 
 export default function AuthNavbar() {
     const navigate = useNavigate();
@@ -75,10 +33,8 @@ export default function AuthNavbar() {
     const userRole = useSelector((state) => state.user.cur);
     const currentLanguage = i18n.language;
     const user = useSelector((state) => state.user);
-    const login = useSelector((state) => state.user.sub);
     const roles = useSelector((state) => state.user.roles);
     const dispatch = useDispatch();
-    const [currentTheme, setCurrentTheme] = useState(guestTheme);
     const [anchorElRole, setAnchorElRole] = React.useState(null);
     const [anchorElLanguage, setAnchorElLanguage] = React.useState(null);
 
@@ -91,60 +47,26 @@ export default function AuthNavbar() {
         setAnchorElRole(null);
     };
 
-    useEffect(() => {
-        if (userRole === ROLES.ADMIN) {
-            setCurrentTheme(adminTheme);
-        } else if (userRole === ROLES.CHEMIST) {
-            setCurrentTheme(chemistTheme);
-        } else if (userRole === ROLES.PATIENT) {
-            setCurrentTheme(patientTheme);
-        }
-    }, [userRole]);
-
-    const [dialogOpen, setDialogOpen] = useState(false);
-
-    const accept = () => {
-        dispatch(logout());
-        navigate(Pathnames.public.login);
-    };
-
-    const reject = () => {
-        setDialogOpen(false);
-    };
-
     const location = useLocation();
 
     let current = "";
 
     return (
-        <ThemeProvider theme={currentTheme}>
             <Box sx={{flexGrow: 1}}>
                 <AppBar position="static">
                     <Toolbar>
                         <Typography
                             onClick={() => navigate(Pathnames.auth.landing)}
-                            variant="h6"
+                            variant="h5"
                             component="div"
                             sx={{flexGrow: 1, cursor: "pointer"}}
                         >
                             {t("internet_pharmacy")}
                         </Typography>
-                        {userRole === ROLES.ADMIN && (
-                            <Button
-                                color="inherit"
-                                onClick={() => navigate(Pathnames.admin.accounts)}
-                            >
-                                {t("accounts")}
-                            </Button>
-                        )}
-                        <Box>
-                            <Typography>{login}</Typography>
-                            <Typography>{userRole}</Typography>
-                        </Box>
                         <IconButton color="inherit" onClick={(e) => {
                             setAnchorElRole(e.currentTarget);
                         }}>
-                            <ManageAccountsIcon/>
+                            <LoopIcon/>
                             <Menu
                                 id="simple-menu"
                                 anchorEl={anchorElRole}
@@ -223,26 +145,10 @@ export default function AuthNavbar() {
                                 ))}
                             </Menu>
                         </IconButton>
-                        <IconButton
-                            color="inherit"
-                            onClick={() => {
-                                setDialogOpen(true);
-                            }}
-                        >
-                            <Logout/>
-                        </IconButton>
-                        <ConfirmationDialog
-                            open={dialogOpen}
-                            title={t("confirm_logout")}
-                            actions={[
-                                {label: t("logout"), handler: accept, color: "primary"},
-                                {label: t("cancel"), handler: reject, color: "secondary"},
-                            ]}
-                            onClose={() => setDialogOpen(false)}
-                        />
+
                     </Toolbar>
                 </AppBar>
-                <Breadcrumbs separator=">" aria-label="breadcrumb">
+                <Breadcrumbs sx={{margin: "10px 20px"}} separator=">" aria-label="breadcrumb">
                     {location.pathname
                         .split("/")
                         .filter((crumb) => crumb !== "")
@@ -266,6 +172,5 @@ export default function AuthNavbar() {
                         })}
                 </Breadcrumbs>
             </Box>
-        </ThemeProvider>
     );
 }
--- front-UI/src/layout/AuthLayout.js
+++ front-UI/src/layout/AuthLayout.js
@@ -1,22 +1,78 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import AuthNavbar from "../components/AuthNavbar";
 import {useLocation, useNavigate} from "react-router-dom";
 import jwtDecode from "jwt-decode";
 import {useTranslation} from "react-i18next";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {toast, ToastContainer} from "react-toastify";
 import {Pathnames} from "../router/Pathnames";
 import {logout} from "../redux/UserSlice";
 import {login as loginDispatch} from "../redux/UserSlice";
-import {ACCESS_LEVEL, JWT_TOKEN} from "../constants/Constants";
+import {ACCESS_LEVEL, JWT_TOKEN, ROLES} from "../constants/Constants";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import {Box} from "@mui/system";
+import AuthSidebar from "../components/AuthSidebar";
+import {blue, pink, purple} from "@mui/material/colors";
+import {colors} from "@mui/material";
+
+const guestTheme = createTheme({
+    palette: {
+        primary: blue,
+    },
+    typography: {
+        fontFamily: ["Lato", "sans-serif"].join(","),
+    },
+});
+
+const adminTheme = createTheme({
+    palette: {
+        primary: {
+            main: colors.red[800],
+        }
+    },
+    typography: {
+        fontFamily: ["Lato", "sans-serif"].join(","),
+    },
+});
+
+const chemistTheme = createTheme({
+    palette: {
+        primary: purple,
+    },
+    typography: {
+        fontFamily: ["Lato", "sans-serif"].join(","),
+    },
+});
+
+const patientTheme = createTheme({
+    palette: {
+        primary: pink,
+    },
+    typography: {
+        fontFamily: ["Lato", "sans-serif"].join(","),
+    },
+});
+
 
 function AuthLayout({children}) {
 
     const location = useLocation();
+    const userRole = useSelector((state) => state.user.cur);
     const token = localStorage.getItem(JWT_TOKEN);
     const navigate = useNavigate();
     const {t} = useTranslation();
     const dispatch = useDispatch();
+    const [currentTheme, setCurrentTheme] = useState(guestTheme);
+
+    useEffect(() => {
+        if (userRole === ROLES.ADMIN) {
+            setCurrentTheme(adminTheme);
+        } else if (userRole === ROLES.CHEMIST) {
+            setCurrentTheme(chemistTheme);
+        } else if (userRole === ROLES.PATIENT) {
+            setCurrentTheme(patientTheme);
+        }
+    }, [userRole]);
 
     useEffect(() => {
         try {
@@ -44,11 +100,16 @@ function AuthLayout({children}) {
     }, [location.pathname])
 
     return (
-        <>
-            <AuthNavbar/>
-            {children}
-            <ToastContainer/>
-        </>
+        <ThemeProvider theme={currentTheme}>
+            <Box sx={{display: 'flex', position: 'relative', alignItems: 'flex-start'}}>
+                <AuthSidebar/>
+                <Box sx={{height: '100%', width: '100%'}}>
+                    <AuthNavbar/>
+                    {children}
+                </Box>
+                <ToastContainer/>
+            </Box>
+        </ThemeProvider>
     );
 }
 
