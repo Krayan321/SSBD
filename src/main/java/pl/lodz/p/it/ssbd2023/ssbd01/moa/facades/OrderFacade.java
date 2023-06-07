02b32da15011920ee7b7032362ea87595e1e1501
229979 <229979@edu.p.lodz.pl>
2023-06-07 14:36:17 +0200

    SSBD202301-201, orderConverter, permissions changed

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd01/moa/facades/OrderFacade.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd01/moa/facades/OrderFacade.java
@@ -1,6 +1,8 @@
 package pl.lodz.p.it.ssbd2023.ssbd01.moa.facades;
 
 import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -27,28 +29,28 @@ public class OrderFacade extends AbstractFacade<Order> {
   }
 
   @Override
-  @DenyAll
+  @PermitAll
   public List<Order> findAll() {
     return super.findAll();
   }
 
   @Override
-  @DenyAll
+  @RolesAllowed("createOrder")
   public void create(Order order) {
     super.create(order);
   }
 
   @Override
-  @DenyAll
+  @PermitAll
   public void edit(Order order) {
     super.edit(order);
   }
   @Override
-  @DenyAll
+  @PermitAll
   public void editAndRefresh(Order order) {
     super.editAndRefresh(order);
   }
-  @DenyAll
+  @PermitAll
   public Optional<Order> find(Long id) {
     return super.find(id);
   }
