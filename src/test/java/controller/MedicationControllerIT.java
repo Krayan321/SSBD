1ea028fd87aea452afa873b35ecb66af9e0ea60c
Jakub Glegola <krayan321@gmail.com>
2023-06-10 12:09:36 +0200

    SSBD202301-197 - getMedications test

--- src/test/java/controller/MedicationControllerIT.java
+++ src/test/java/controller/MedicationControllerIT.java
@@ -9,38 +9,32 @@ import org.junit.jupiter.api.*;
 
 import static controller.dataForTests.*;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.Matchers.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MedicationControllerIT extends BaseTest {
 
-    static String adminJwt;
+    static String chemistJwt;
 
     @BeforeAll
     static void setUp() throws InterruptedException {
         System.out.println(getApiRoot());
-        String jsonJwt =
-                given()
-                        .contentType("application/json")
-                        .body(adminLoginDto)
-                        .log()
-                        .all()
-                        .post(getApiRoot() + "/auth/login")
-                        .then()
-                        .log()
-                        .all()
-                        .statusCode(Response.Status.OK.getStatusCode())
-                        .extract()
-                        .response()
-                        .asString();
+        chemistJwt = given()
+                .contentType("application/json")
+                .body(chemistLoginDto)
+                .log().all()
+                .post(getApiRoot() + "/auth/login")
+                .then().log().all()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().jsonPath().getString("jwtToken");
 
-        adminJwt = jsonJwt.substring(jsonJwt.indexOf(":") + 2, jsonJwt.length() - 2);
-
-        RestAssured.requestSpecification =
-                new RequestSpecBuilder()
-                        .setContentType(ContentType.JSON)
-                        .setAccept(ContentType.JSON)
-                        .log(LogDetail.ALL)
-                        .build();
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setAccept(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
     }
 
 
@@ -48,7 +42,7 @@ public class MedicationControllerIT extends BaseTest {
     @Order(1)
     public void addCategory() {
         given()
-                .header("Authorization", "Bearer " + adminJwt)
+                .header("Authorization", "Bearer " + chemistJwt)
                 .body(categoryDto)
                 .post(getApiRoot() + "/category/add-category")
                 .then()
@@ -58,32 +52,43 @@ public class MedicationControllerIT extends BaseTest {
     @Order(2)
     public void addMedication_correct() {
         given()
-                .header("Authorization", "Bearer " + adminJwt)
+                .header("Authorization", "Bearer " + chemistJwt)
                 .body(addMedicationDto)
                 .post(getApiRoot() + "/medication/add-medication")
                 .then()
                 .statusCode(Response.Status.CREATED.getStatusCode());
     }
 
-    @Test
-    @Order(3)
-    public void addMedication_sameName() {
-        given()
-                .header("Authorization", "Bearer " + adminJwt)
-                .body(addMedicationDto)
-                .post(getApiRoot() + "/medication/add-medication")
-                .then()
-                .statusCode(Response.Status.CONFLICT.getStatusCode());
-    }
+//    @Test
+//    @Order(3)
+//    public void addMedication_sameName() {
+//        given()
+//                .header("Authorization", "Bearer " + adminJwt)
+//                .body(addMedicationDto)
+//                .post(getApiRoot() + "/medication/add-medication")
+//                .then()
+//                .statusCode(Response.Status.CONFLICT.getStatusCode());
+//    }
+//
+//    @Test
+//    @Order(4)
+//    public void addMedication_noSuchCategory() {
+//        given()
+//                .header("Authorization", "Bearer " + adminJwt)
+//                .body(addMedicationDto)
+//                .post(getApiRoot() + "/medication/add-medication")
+//                .then()
+//                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+//    }
 
     @Test
-    @Order(4)
-    public void addMedication_noSuchCategory() {
+    @Order(5)
+    public void getMedications_correct() {
         given()
-                .header("Authorization", "Bearer " + adminJwt)
-                .body(addMedicationDto)
-                .post(getApiRoot() + "/medication/add-medication")
+                .header("authorization", "Bearer " + chemistJwt)
+                .get(getApiRoot() + "/account/")
                 .then()
-                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+                .statusCode(Response.Status.OK.getStatusCode())
+                .body("$", hasSize(equalTo(5)));
     }
 }
