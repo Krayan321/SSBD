47800905d1c4041b4f02a525ada826b7cc209fdf
236641 Rubacha Jan <236641@edu.p.lodz.pl>
2023-06-13 22:59:55 +0200

    SSBD202301-250 automated generation of report

--- bin/deploy.ps1
+++ bin/deploy.ps1
@@ -9,7 +9,7 @@ npm run build --prefix "$PROJECT_ROOT\front-UI"
 
 #deploy backend
 $WAR = Get-ChildItem -Path "$PROJECT_ROOT\target" *.war | Select-Object -First 1
-..\Payara\payara6_3\bin\asadmin --host 10.31.201.3 --port 4848 --user admin --passwordfile "$PROJECT_ROOT\config\payara-password" redeploy --name "ssbd01" "$PROJECT_ROOT\target\$WAR"
+asadmin --host 10.31.201.3 --port 4848 --user admin --passwordfile "$PROJECT_ROOT\config\payara-password" redeploy --name "ssbd01" "$PROJECT_ROOT\target\$WAR"
 
 #deploy frontend
 cd C:\
--- src/test/java/controller/CategoryControllerIT.java
+++ src/test/java/controller/CategoryControllerIT.java
@@ -1,51 +1,51 @@
-//package controller;
-//
-//import io.restassured.RestAssured;
-//import io.restassured.builder.RequestSpecBuilder;
-//import io.restassured.filter.log.LogDetail;
-//import io.restassured.http.ContentType;
-//import jakarta.ws.rs.core.Response;
-//import org.junit.jupiter.api.*;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//
-//import static controller.dataForTests.*;
-//import static io.restassured.RestAssured.given;
-//
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//@Testcontainers
-//public class CategoryControllerIT extends BaseTest {
-//
-//    static String chemistJwt;
-//
-//    @BeforeAll
-//    static void setUp() throws InterruptedException {
-//        System.out.println(getApiRoot());
-//        chemistJwt = given()
-//                .contentType("application/json")
-//                .body(chemistLoginDto)
-//                .log().all()
-//                .post(getApiRoot() + "/auth/login")
-//                .then().log().all()
-//                .statusCode(Response.Status.OK.getStatusCode())
-//                .extract().jsonPath().getString("jwtToken");
-//
-//        RestAssured.requestSpecification = new RequestSpecBuilder()
-//                .setContentType(ContentType.JSON)
-//                .setAccept(ContentType.JSON)
-//                .log(LogDetail.ALL)
-//                .build();
-//    }
-//
-//
-//
-//    @Test
-//    @Order(1)
-//    public void addCategory() {
-//        given()
-//                .header("Authorization", "Bearer " + chemistJwt)
-//                .body(categoryDto)
-//                .post(getApiRoot() + "/category/add-category")
-//                .then()
-//                .statusCode(Response.Status.CREATED.getStatusCode());
-//    }
-//}
+package controller;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.*;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static controller.dataForTests.*;
+import static io.restassured.RestAssured.given;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Testcontainers
+public class CategoryControllerIT extends BaseTest {
+
+    static String chemistJwt;
+
+    @BeforeAll
+    static void setUp() throws InterruptedException {
+        System.out.println(getApiRoot());
+        chemistJwt = given()
+                .contentType("application/json")
+                .body(chemistLoginDto)
+                .log().all()
+                .post(getApiRoot() + "/auth/login")
+                .then().log().all()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().jsonPath().getString("jwtToken");
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setAccept(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+    }
+
+
+
+    @Test
+    @Order(1)
+    public void addCategory() {
+        given()
+                .header("Authorization", "Bearer " + chemistJwt)
+                .body(categoryDto)
+                .post(getApiRoot() + "/category/add-category")
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode());
+    }
+}
