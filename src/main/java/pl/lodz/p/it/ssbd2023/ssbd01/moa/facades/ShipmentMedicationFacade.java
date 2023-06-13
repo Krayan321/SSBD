8d2bd046f07350caa00b0be524da3f74d3d4ebd8
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-06-13 16:02:58 +0200

    SSBD202301-195 minor fix

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd01/moa/facades/ShipmentMedicationFacade.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd01/moa/facades/ShipmentMedicationFacade.java
@@ -1,33 +1,38 @@
 package pl.lodz.p.it.ssbd2023.ssbd01.moa.facades;
 
-import jakarta.annotation.security.DenyAll;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
-import java.util.List;
 import pl.lodz.p.it.ssbd2023.ssbd01.common.AbstractFacade;
 import pl.lodz.p.it.ssbd2023.ssbd01.entities.ShipmentMedication;
 
+import java.util.List;
+
 @Stateless
 public class ShipmentMedicationFacade extends AbstractFacade<ShipmentMedication> {
-  @PersistenceContext(unitName = "ssbd01moaPU")
-  private EntityManager em;
+    @PersistenceContext(unitName = "ssbd01moaPU")
+    private EntityManager em;
 
-  @Override
-  protected EntityManager getEntityManager() {
-    return em;
-  }
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
 
-  public ShipmentMedicationFacade() {
-    super(ShipmentMedication.class);
-  }
+    public ShipmentMedicationFacade() {
+        super(ShipmentMedication.class);
+    }
 
-  public ShipmentMedication findLatestByMedication(Long medicationId) {
-    TypedQuery<ShipmentMedication> query = em.createNamedQuery("shipmentMedication.findLatestShipmentMedicationForGivenMedication", ShipmentMedication.class);
-    query.setParameter(1, medicationId);
-    return query.getSingleResult();
+    public ShipmentMedication findLatestByMedication(Long medicationId) {
+        TypedQuery<ShipmentMedication> query = em.createNamedQuery("shipmentMedication.findLatestShipmentMedicationForGivenMedication", ShipmentMedication.class);
+        query.setParameter("medicationId", medicationId);
+        List<ShipmentMedication> shipmentMedications = query.getResultList();
+        if (shipmentMedications.isEmpty())
+            return null;
+        else {
+            return shipmentMedications.get(0);
+        }
 
-  }
+    }
 
 }
